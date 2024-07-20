33f7a9c6c4a803ce07d031e027d895448a58b321
Sean Stocker <sean.stocker14@gmail.com>
2024-07-20 23:12:00 +0100

    Database Setup
    docs: Updated README.md
    feat: Added postgres service to docker-compose
    feat: Create prisma schema with user model
    feat: Updated app.ts to use prisma instead of pg
    feat: Added routes to test the database out
    feat: Implemented graceful shutdown of prisma client with process.on handlers

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Chatster
 
 Simple chat app with React.js and Express.js
+
+## Get Started
+
+Build the image and start the containers:
+
+```bash
+docker-compose up --build -d
+```
+
+On first use, prepare database by setting up prisma migrations:
+
+```bash
+docker exec -it chatster-server npx prisma migrate dev --name init
+```
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,73 @@
 import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
 
+// Load environment variables from .env file
+dotenv.config();
+const prisma = new PrismaClient();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
 
 // Define a route
-app.get("/", (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
+  const result =
+    await prisma.$queryRaw`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';`;
+  console.log(result);
   res.send("Hello, TypeScript!");
 });
 
+app.post("/user/add", async (req, res) => {
+  try {
+    const user = {
+      name: req.body.name,
+      email: req.body.email,
+    };
+
+    const result = await prisma.user.create({
+      data: user,
+    });
+
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error creating user");
+  }
+});
+
+// Check database connection
+app.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching users");
+  }
+});
+
 // Start the server
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
+});
+
+process.on("SIGINT", async () => {
+  console.log("SIGINT signal received: closing HTTP server");
+  server.close(async () => {
+    console.log("HTTP server closed");
+    await prisma.$disconnect();
+    console.log("Prisma Client disconnected");
+    process.exit(0);
+  });
+});
+
+process.on("SIGTERM", async () => {
+  console.log("SIGTERM signal received: closing HTTP server");
+  server.close(async () => {
+    console.log("HTTP server closed");
+    await prisma.$disconnect();
+    console.log("Prisma Client disconnected");
+    process.exit(0);
+  });
 });
