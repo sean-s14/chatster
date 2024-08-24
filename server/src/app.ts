db63d8c92f1770706b68794368b3aaa6d563aa22
Sean Stocker <sean.stocker14@gmail.com>
2024-08-24 18:18:03 +0100

    feat: Added logging with Morgan middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application, Request, Response } from "express";
 import helmet from "helmet";
+import morgan from "morgan";
 import userRoutes from "./routes/userRoutes";
 import { errorHandler } from "./middleware/errorMiddleware";
 
 const app: Application = express();
 
 app.use(helmet());
+app.use(morgan("dev"));
 app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use(errorHandler);
